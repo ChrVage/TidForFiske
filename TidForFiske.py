0ab014b46cd22ba1b81b28bbdf02a4ec5f277aba
Christian Våge <Chr@Vage.com>
2024-05-27 22:04:42 +0200

    Refactor fetch_data function in TidForFiske.py and start getting sun-times

--- TidForFiske.py
+++ TidForFiske.py
@@ -5,13 +5,10 @@ import pytz
 import json
 from xml.etree import ElementTree as ET
 
-# This fun
+# 
 def fetch_data(url, params=None, headers=None):
     """Unified data fetching function to handle both XML and JSON responses."""
     response = requests.get(url, params=params, headers=headers)
-
-# Til havnivå, er alle parameterene med? Sjekk i postman. Når 
-
     if response.status_code == 200:
         content_type = response.headers.get('Content-Type', '')
         if 'application/json' in content_type:
@@ -28,14 +25,13 @@ def fetch_data(url, params=None, headers=None):
                 return None
     return None
 
-# 
 def get_time_range(num_days, timezone='UTC'):
-    """Generate from and to time strings."""
+    """Generate from and to time strings. To be used for fetching data for a specific number of days."""
     utc_now = datetime.now(pytz.utc)
     return utc_now.strftime("%Y-%m-%dT%H:%M:%S+00:00"), (utc_now + timedelta(days=num_days)).strftime("%Y-%m-%dT%H:%M:%S+00:00")
 
 def process_high_tides(data, timezone):
-    """Process high tides data."""
+    """Process high tides data. To return a list of high tides."""
     high_tides = []
     for waterlevel in data.findall(".//waterlevel[@flag='high']"):
         time_obj = datetime.fromisoformat(waterlevel.get("time").replace('Z', '+00:00')).astimezone(timezone)
@@ -48,7 +44,7 @@ def process_high_tides(data, timezone):
     return high_tides
 
 def process_forecasts(data, forecast_type):
-    """Process weather and ocean forecasts."""
+    """Process weather and ocean forecasts. To return a list of forecasts."""
     forecasts = []
     for entry in data["properties"]["timeseries"]:
         forecast = {"time": entry["time"]}
@@ -59,16 +55,17 @@ def process_forecasts(data, forecast_type):
         forecasts.append(forecast)
     return forecasts
 
-def get_data(latitude, longitude, num_days):
-    """Fetch and combine data from different sources."""
+def read_apis(latitude, longitude, num_days):
+    """Fetch and combine data from different sources. Such as tide, weather, and ocean forecasts.""" 
     timezone = pytz.timezone('CET')
     fromtime, totime = get_time_range(num_days)
     
     # URLs and parameters
     tide_api_url = "https://api.sehavniva.no/tideapi.php"
+    sunrise_api_url = "https://api.met.no/weatherapi/sunrise/2.0/.json"
     weather_api_url = "https://api.met.no/weatherapi/locationforecast/2.0/compact"
     ocean_api_url = "https://api.met.no/weatherapi/oceanforecast/2.0/complete"
-    sunrise_api_url = "https://api.met.no/weatherapi/sunrise/2.0/.json"
+
     
     common_params = {"lat": latitude, "lon": longitude, "fromtime": fromtime, "totime": totime}
     headers = {'User-Agent': 'TidForFiske (Chr@Vage.com)'}
@@ -77,15 +74,15 @@ def get_data(latitude, longitude, num_days):
     tide_data = fetch_data(tide_api_url, params=common_params)
     weather_data = fetch_data(weather_api_url, headers=headers, params={"lat": latitude, "lon": longitude})
     ocean_data = fetch_data(ocean_api_url, headers=headers, params={"lat": latitude, "lon": longitude})
-    sun_data = fetch_data(sunrise_api_url, headers=headers, params={"lat": latitude, "lon": longitude})
-    
+    sun_data = fetch_data(sunrise_api_url, params={"lat": latitude, "lon": longitude})
+    print(sun_data)
+ 
     # Process data
     high_tides = process_high_tides(tide_data, timezone) if tide_data else []
     weather_forecast = process_forecasts(weather_data, 'weather') if weather_data else []
     ocean_forecast = process_forecasts(ocean_data, 'ocean') if ocean_data else []
-    sun_data = process_forecasts(sun_data, 'sunrise') if sun_data else []
     
-    return high_tides, weather_forecast, ocean_forecast, sun_data
+    return high_tides, weather_forecast, ocean_forecast
 
 def create_excel(data, file_name='output.xlsx'):
     """Generate Excel file from data."""
@@ -95,13 +92,9 @@ def create_excel(data, file_name='output.xlsx'):
 
 # Configuration and data fetching
 config = json.load(open('TidForFiske_config.json', 'r', encoding='utf-8'))
-latitude, longitude, time_start, time_end, duration_prep, duration_fish, duration_home, num_days, mode = config.get("latitude", 59.9), config.get("longitude", 5.0), config.get("time_start", 6), config.get("time_end", 18), config.get("duration_prep", 1), config.get("duration_fish", 4), config.get("duration_home", 1), config.get("NumDays", 30), config.get("mode", "fishing")
+latitude, longitude, num_days = config.get("latitude", 60), config.get("longitude", 5.0), config.get("NumDays", 30)
 
-high_tides, weather_forecast, ocean_forecast, sun_data = get_data(latitude, longitude, num_days)
+high_tides, weather_forecast, ocean_forecast = read_apis(latitude, longitude, num_days)
 
 # Further processing and Excel creation can follow based on the obtained data
-create_excel(weather_forecast + ocean_forecast, "zz_FiskeTid.xlsx")
-create_excel(high_tides , "zz_Flo.xlsx")
-create_excel(sun_data, "zz_SunriseSunset.xlsx")
-
-
+create_excel(high_tides + weather_forecast + ocean_forecast, "zz_FiskeTid.xlsx")
